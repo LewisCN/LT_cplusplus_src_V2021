fd07c8f906fd89a6ef5f743223c41e1f64ee0ac7
384214554@qq.com <384214554@qq.com>
2021-08-21 09:55:00 +0800

    create main.cpp and config.h file.

---
+++ cplusplus_stl/cplusplus_stl/config.h
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#ifdef ENABLE_LT_DEBUG
+#define PRINT_DEBUG(...)  std::cout
+#else
+#define PRINT_DEBUG(...) do{}while(0)
+#endif
---
+++ cplusplus_stl/cplusplus_stl/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "config.h"
+
+
+int main(int argc, char*argv[])
+{
+	
+
+	
+	return 0;
+}
