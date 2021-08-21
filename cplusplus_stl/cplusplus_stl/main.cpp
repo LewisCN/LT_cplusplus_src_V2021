a3a1ef43dd169d1e14dd25abd8ea06cd404841e1
384214554@qq.com <384214554@qq.com>
2021-08-21 15:52:18 +0800

    vector的一些用法

--- cplusplus_stl/cplusplus_stl/main.cpp
+++ cplusplus_stl/cplusplus_stl/main.cpp
@@ -3,22 +3,50 @@
 #include <vector>
 #include <iterator>
 #include <algorithm>
+#include <numeric>
+#include <functional>
 #include <array>
 
 template<typename T>
 void output(T &t) { std::cout << t << ' '; }
 
+namespace vector_unit {
+	using std::vector;
+
+template<class _Value_Type>
+void vector_unit(int t, _Value_Type start) {
+	vector<_Value_Type> vec;
+
+	for (int i = 0; i < t; i++) {
+		vec.push_back(start + i);
+	}
+	std::cout << "@mark01" << std::endl;
+	std::for_each(vec.begin(), vec.end(), output<_Value_Type>);
+}
+template<typename _Value_Type>
+void vector_unit(int t) {
+	vector<_Value_Type> vec;
+	for (int i = 0; i < t; i++) {
+		vec.push_back(i);
+	}
+	std::cout << "@mark02" << std::endl;
+	std::for_each(vec.begin(), vec.end(), output<_Value_Type>);
+}
+
+}
+
 int main(int argc, char*argv[])
 {
 	using namespace std;
-	vector<int> v(10, 3);
-
-	for (int i = 0; i < v.size(); i++) {
-		v.push_back(i);
-	}
-
-	for_each(v.begin(), v.end(), output<int>);
-
+	//using namespace vector_unit;
+	/*直接申明命名空间的函数*/
+	// using vector_unit::vector_unit;
+	// vector_unit<int, int>(10, 3);
+	vector_unit::vector_unit<int>(10, 3);
 	
+
+
+
+
 	return 0;
 }
